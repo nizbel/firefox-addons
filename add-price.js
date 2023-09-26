77c164873d120fa6e5e49e6e677e49d01650bffe
Guilherme de Deus <kingbowserii@gmail.com>
2023-09-26 17:59:12 -0300

    Calculating prices whenever listings are refreshed

--- add-price.js
+++ add-price.js
@@ -19,19 +19,31 @@
     const listings = document.getElementsByClassName("sc-1tt2vbg-3 eFJncP");
 
     if (listings.length > 0) {
-      // Pick listings
-      calculateListings(listings);
+      for (let i = 0; i < listings.length; i++) {
+        const listing = listings[i];
+        const observer = new MutationObserver((records, observer) => {
+          calculateListings([listing,]);
+        });
+        observer.observe(listing, {childList: true, subtree: true});
+      }
     } else {
-      // Look for data in details page
-      const areaIcon = document.getElementsByClassName("icon-scubierta")[0];
-      const m2Size = areaIcon.parentElement.textContent.trim().replace(/\D/g, '');
-      
-      const priceElement = document.getElementsByClassName("price-value")[0];
-      const price = priceElement.children[0].children[0].textContent.trim().replace(/\D/g, '');
-      
-      const pricePerM2 = parseInt(price) / m2Size;
+      const observer = new MutationObserver((records, observer) => {
+        // Look for data in details page
+        const priceElement = document.getElementsByClassName("price-value")[0];
+        if (priceElement.children[0].children[0].textContent.indexOf("/m²") > -1) {
+            return;
+        }
+        const price = priceElement.children[0].children[0].textContent.trim().replace(/\D/g, '');
 
-      priceElement.children[0].children[0].textContent += " (" + formatter.format(pricePerM2) + "/m²)";
+        const areaIcon = document.getElementsByClassName("icon-scubierta")[0];
+        const m2Size = areaIcon.parentElement.textContent.trim().replace(/\D/g, '');
+        
+        const pricePerM2 = parseInt(price) / m2Size;
+
+        priceElement.children[0].children[0].textContent += " (" + formatter.format(pricePerM2) + "/m²)";
+      });
+
+      observer.observe(document, {childList: true, subtree: true});
     }
   }
       
@@ -39,6 +51,13 @@
   function calculateListings(listings) {
     for (let i = 0; i < listings.length; i++) {
       let currentElement = listings[i];
+
+      // Look for price
+      let priceElement = currentElement.getElementsByClassName("sc-12dh9kl-4 hbUMaO");
+
+      if (priceElement.length !== 1 || priceElement[0].textContent.indexOf("/m²") > -1) {
+          continue;
+      }
 
       // Look for size
       let sizeElement = currentElement.getElementsByClassName("sc-1uhtbxc-1 dRoEma");
@@ -56,13 +75,6 @@
       // Pick just the numeric part
       m2Size = m2Size.textContent.trim().split(" ")[0];
   
-      // Look for price
-      let priceElement = currentElement.getElementsByClassName("sc-12dh9kl-4 hbUMaO");
-
-      if (priceElement.length !== 1) {
-          continue;
-      }
-
       // Pick the first element if available
       priceElement = priceElement[0];
 
@@ -74,7 +86,24 @@
       priceElement.textContent = priceElement.textContent + " (" + formatter.format(pricePerM2) + "/m²)";
     }
   }
-  
+
+  function logChanges(records, observer) {
+    for (const record of records) {
+      for (const addedNode of record.addedNodes) {
+        alert(`Added: ${addedNode.textContent}\n${log.textContent}`);
+      }
+      for (const removedNode of record.removedNodes) {
+        alert(`Removed: ${removedNode.textContent}\n${log.textContent}`);
+      }
+      // if (record.target.childNodes.length === 0) {
+      //   alert(`Disconnected\n${log.textContent}`);
+      //   observer.disconnect();
+      // }
+      alert(record.target.childNodes.length);
+    }
+    alert(observer);
+  }
+
   /**
    * Listen for messages from the background script.
    */
